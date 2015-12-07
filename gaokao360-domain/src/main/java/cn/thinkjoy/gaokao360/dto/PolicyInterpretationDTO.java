eb3ca8c425e0709cd535d5593a195b7b73e1f839
yyp <wuhun0301@qq.com>
2015-12-07 17:02:37 +0800

    高考热点和政策解读相关文件

--- gaokao360-domain/src/main/java/cn/thinkjoy/gaokao360/dto/PolicyInterpretationDTO.java
+++ gaokao360-domain/src/main/java/cn/thinkjoy/gaokao360/dto/PolicyInterpretationDTO.java
@@ -7,4 +7,21 @@ import cn.thinkjoy.gaokao360.domain.PolicyInterpretation;
  */
 public class PolicyInterpretationDTO extends PolicyInterpretation {
     private String province;
+    private String admissionBatch;
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getAdmissionBatch() {
+        return admissionBatch;
+    }
+
+    public void setAdmissionBatch(String admissionBatch) {
+        this.admissionBatch = admissionBatch;
+    }
 }
