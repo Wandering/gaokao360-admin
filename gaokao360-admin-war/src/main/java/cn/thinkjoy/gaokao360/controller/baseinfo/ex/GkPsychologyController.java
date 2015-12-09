2ee1a7cbc5bb5574d56d02bd94a4e516d720db92
yyp <wuhun0301@qq.com>
2015-12-09 13:16:16 +0800

    高考心理和名师讲堂相关文件

---
+++ gaokao360-admin-war/src/main/java/cn/thinkjoy/gaokao360/controller/baseinfo/ex/GkPsychologyController.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2013-2014, thinkjoy Inc. All Rights Reserved.
+ *
+ * Project Name: gaokao360
+ * $Id:  VideoCourseController.java 2015-12-04 13:22:05 $
+ */
+
+package cn.thinkjoy.gaokao360.controller.baseinfo.ex;
+
+import cn.thinkjoy.common.domain.view.BizData4Page;
+import cn.thinkjoy.common.managerui.controller.AbstractAdminController;
+import cn.thinkjoy.gaokao360.service.IVideoCourseService;
+import cn.thinkjoy.gaokao360.service.ex.IGkPsychologyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping(value="/admin/gaokao360/ex")
+public class GkPsychologyController extends AbstractAdminController<IGkPsychologyService>{
+
+
+    @Autowired
+    private IGkPsychologyService gkPsychologyService;
+
+    /**
+     * 页面主请求
+     * @param request
+     * @param response
+     * @return  返回菜单数据、表格描述元数据、当前主描述  如本页面为org
+     */
+    @RequestMapping(value="/gkpsychology")
+    public ModelAndView renderMainView(HttpServletRequest request,HttpServletResponse response){
+
+
+        return doRenderMainView(request, response);
+    }
+
+    /**
+     * 获取所有的组织信息
+     * @return
+     */
+    @RequestMapping(value="/gkpsychologys")
+    @ResponseBody
+    public BizData4Page findAllVideoCourses(HttpServletRequest request,HttpServletResponse response){
+        return doPage(request, response);
+    }
+
+    @Override
+    protected IGkPsychologyService getMainService() {
+        return gkPsychologyService;
+    }
+
+    @Override
+    protected String getBizSys() {
+        return "gaokao360/ex";
+    }
+
+    @Override
+    protected String getMainObjName() {
+        return "gkpsychologys";
+    }
+
+    @Override
+    protected String getViewTitle() {
+        return "视频详情";
+    }
+
+    @Override
+    protected String getParentTitle() {
+        return "数据模块";
+    }
+
+    @Override
+    public boolean getEnableDataPerm() {
+        return true;
+    }
+}
---
+++ gaokao360-service/src/test/java/cn/thinkjoy/Test.java
@@ -0,0 +1,8 @@
+package cn.thinkjoy;
+
+/**
+ * Created by admin on 2015/12/9.
+ */
+public class Test {
+
+}
