a9ffccf1d0f5904e06391cb2dacd90e26582eabb
yyp <wuhun0301@qq.com>
2015-12-14 09:47:46 +0800

    高考热点和政策解读相关文件

---
+++ gaokao360-admin-war/src/main/java/cn/thinkjoy/gaokao360/controller/baseinfo/ex/VideoSectionExController.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2013-2014, thinkjoy Inc. All Rights Reserved.
+ *
+ * Project Name: gaokao360
+ * $Id:  VideoSectionController.java 2015-12-04 13:22:05 $
+ */
+
+package cn.thinkjoy.gaokao360.controller.baseinfo.ex;
+
+import cn.thinkjoy.common.domain.view.BizData4Page;
+import cn.thinkjoy.common.managerui.controller.AbstractAdminController;
+import cn.thinkjoy.gaokao360.domain.VideoSection;
+import cn.thinkjoy.gaokao360.service.IVideoSectionService;
+import cn.thinkjoy.gaokao360.service.ex.IVideoSectionExService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping(value="/admin/gaokao360/ex")
+public class VideoSectionExController extends AbstractAdminController<IVideoSectionExService>{
+
+
+    @Autowired
+    private IVideoSectionExService videoSectionExService;
+
+    /**
+     * 页面主请求
+     * @param request
+     * @param response
+     * @return  返回菜单数据、表格描述元数据、当前主描述  如本页面为org
+     */
+    @RequestMapping(value="/videosection")
+    public ModelAndView renderMainView(HttpServletRequest request,HttpServletResponse response){
+
+
+        return doRenderMainView(request, response);
+    }
+
+
+
+    /**
+     * 获取所有的组织信息
+     * @return
+     */
+    @RequestMapping(value="/videosections")
+    @ResponseBody
+    public BizData4Page findAllVideoSections(HttpServletRequest request,HttpServletResponse response){
+        return doPage(request, response);
+    }
+
+    public VideoSection queryByCourseId(String courseId){
+        return videoSectionExService.queryByCourseId(courseId);
+    }
+    @Override
+    protected IVideoSectionExService getMainService() {
+        return videoSectionExService;
+    }
+
+    @Override
+    protected String getBizSys() {
+        return "gaokao360";
+    }
+
+    @Override
+    protected String getMainObjName() {
+        return "videosection";
+    }
+
+    @Override
+    protected String getViewTitle() {
+        return "视频描述";
+    }
+
+    @Override
+    protected String getParentTitle() {
+        return "数据模块";
+    }
+
+    @Override
+    public boolean getEnableDataPerm() {
+        return true;
+    }
+}
