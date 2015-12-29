53dfa2be9ddd86855940ca8838da5b950dccbe98
yyp <wuhun0301@qq.com>
2015-12-29 14:18:44 +0800

    添加院校招生信息接口

---
+++ gaokao360-service/src/main/java/cn/thinkjoy/gaokao360/dao/IDataDictDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2013-2014, thinkjoy Inc. All Rights Reserved.
+ *
+ * Project Name: gaokao360
+ * $Id:  DataDictDAO.java 2015-12-29 10:29:59 $
+ */
+package cn.thinkjoy.gaokao360.dao;
+
+import cn.thinkjoy.common.dao.IBaseDAO;
+import cn.thinkjoy.gaokao360.domain.DataDict;
+
+public interface IDataDictDAO extends IBaseDAO<DataDict>{
+	
+
+}
