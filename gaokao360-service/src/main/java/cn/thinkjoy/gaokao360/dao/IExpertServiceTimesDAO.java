b8dde9282656388dedd47a119e38894590a2d012
yangyongping <yangyongping@operationadmins-MacBook-Pro.local>
2016-11-15 17:55:36 +0800

    才储接口回调类型区分  api版本升级39

---
+++ gaokao360-service/src/main/java/cn/thinkjoy/gaokao360/dao/IExpertServiceTimesDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2013-2014, thinkjoy Inc. All Rights Reserved.
+ *
+ * Project Name: gaokao360
+ * $Id:  ExpertServiceTimesDAO.java 2016-11-02 11:26:28 $
+ */
+package cn.thinkjoy.gaokao360.dao;
+
+import cn.thinkjoy.common.dao.IBaseDAO;
+import cn.thinkjoy.gaokao360.domain.ExpertServiceTimes;
+
+public interface IExpertServiceTimesDAO extends IBaseDAO<ExpertServiceTimes>{
+	
+
+}
